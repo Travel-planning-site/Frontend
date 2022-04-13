c1b41eff5edcf930c090ac85f16b6e05fa65f80e
knh4437 <knh4437@naver.com>
2022-04-14 03:08:35 +0900

    Updated ResearchPlace
    Add SavedBox and SavedList

---
+++ frontend/src/components/ResultList.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="ResultList">
+      <template>
+        <result-box :ResultList_results="results" @SelectFromResult_box="SelectFromResult_box"></result-box>
+      </template>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ResultList',
+  props: {
+    results: {
+      type: Object,
+      default: () => {
+        return {
+          place_img: '',
+          place_name: '',
+          place_addr: ''
+        }
+      }
+    }
+  },
+  data () {
+    return {
+      selectedResult: {
+        place_img: '',
+        place_name: '',
+        place_addr: ''
+      }
+    }
+  },
+  methods: {
+    SelectFromResult_box: function (result) {
+      console.log('ResultList 담기 발생')
+      this.selectedResult = result
+      this.$emit('SelectFromResult_List', this.selectedResult)
+    }
+  }
+}
+</script>
--- frontend/src/components/SavedList.vue
+++ frontend/src/components/SavedList.vue
@@ -3,9 +3,39 @@
     <b-container fluid>
       <b-row style="margin: 5px;">
         <b-col>
-          <savedplace-box></savedplace-box>
+          <savedplace-box v-if="selected_count" :selectedList="selectedList" @DeleteOnClicked="DeleteOnClicked"></savedplace-box>
         </b-col>
       </b-row>
     </b-container>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'SavedList',
+  props: {
+    selectedList: {
+      type: Object,
+      default: () => {
+        return {
+          place_name: '',
+          place_addr: '',
+          place_img: ''
+        }
+      }
+    },
+    selected_count: {
+      type: Number
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    DeleteOnClicked () {
+      console.log('삭제이벤트')
+    }
+  }
+}
+</script>
