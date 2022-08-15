e299045a51cfd1fd7eb1f82b2444fd699d5db7a6
knh4437 <knh4437@naver.com>
2022-08-15 18:34:31 +0900

    Update GoogleSearch
    장소 검색을 위한 Google Search 및 장소 사진 출력을 위한 Google PlacePhoto

---
+++ frontend/src/components/GoogleSearch.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="GoogleSearch">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GoogleSearch',
+  props: {
+    keyword: {
+      type: String
+    },
+    page: {
+      type: Number
+    }
+  },
+  methods: {
+    googleSearch (keyword, page) {
+      axios.get(
+
+      )
+    }
+  }
+
+}
+</script>
