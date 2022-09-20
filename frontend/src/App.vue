a31005bf54e8f2fec97eec89dba805d7c744b756
knh4437 <knh4437@naver.com>
2022-09-20 19:17:54 +0900

    connected BE

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,30 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
+import { LOCAL_URL } from './url/BackendUrl'
+import axios from 'axios'
+
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      connected: false
+    }
+  },
+  created () {
+    axios.get(LOCAL_URL + '/test')
+      .then(res => {
+        this.connected = res.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 }
 </script>
 
@@ -22,38 +39,41 @@ export default {
 } */
 
 .dotOverlay {
-  position:relative;
-  bottom:50px;
-  border-radius:6px;
+  position: relative;
+  bottom: 50px;
+  border-radius: 6px;
   border: 1px solid #ccc;
-  border-bottom:2px solid #ddd;
-  float:left;
-  font-size:20px;
-  padding:5px;
-  background:#fff;
-  list-style:none;
-  }
+  border-bottom: 2px solid #ddd;
+  float: left;
+  font-size: 20px;
+  padding: 5px;
+  background: #fff;
+  list-style: none;
+}
 
 .dotOverlay:nth-of-type(n) {
-  border:0;
-  box-shadow:0px 1px 2px #888;
-  }
+  border: 0;
+  box-shadow: 0px 1px 2px #888;
+}
 
 .number {
-  font-weight:bold;
-  color:#ee6152;
-  }
+  font-weight: bold;
+  color: #ee6152;
+}
 
 .dotOverlay:after {
-  content:'';
-  position:absolute;
-  margin-left:-6px;
-  left:50%;bottom:-8px;
-  width:11px;height:8px;
-  background:url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white_small.png')}
+  content: "";
+  position: absolute;
+  margin-left: -6px;
+  left: 50%;
+  bottom: -8px;
+  width: 11px;
+  height: 8px;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white_small.png");
+}
 
 .distanceInfo .label {
-  display:inline-block;
-  width:50px;
-  }
+  display: inline-block;
+  width: 50px;
+}
 </style>
