d3f722982c9452c1ac1f4defd5c3203d4c675620
hy <75581904+dhgkdud12@users.noreply.github.com>
2022-11-16 22:08:24 +0900

    오타수정

--- frontend/src/pages/MyPlanBox.vue
+++ frontend/src/pages/MyPlanBox.vue
@@ -52,12 +52,12 @@ export default {
   },
   created () {
     this.plan = this.planData.info
-    this.travles = this.planData.travles
-    this.imgUrl = this.planData.travles[0].startPlaceImg
+    this.travels = this.planData.travels
+    this.imgUrl = this.planData.travels[0].startPlaceImg
   },
   methods: {
     onClickDetail () {
-      this.$router.push({name: 'MyPlanDetails', params: { travles: this.travles }})
+      this.$router.push({name: 'MyPlanDetails', params: { travels: this.travels }})
     },
     onClickDelete () {
       console.log(this.plan.idx)
